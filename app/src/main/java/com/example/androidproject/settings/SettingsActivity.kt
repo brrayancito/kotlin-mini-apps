463f2072081d8b2977d0c63dead6f11667d194eb
Brrayan <brrayanperez10@gmail.com>
2023-11-30 18:46:13 -0400

    Added dataStore dependency

--- app/src/main/java/com/example/androidproject/settings/SettingsActivity.kt
+++ app/src/main/java/com/example/androidproject/settings/SettingsActivity.kt
@@ -1,9 +1,15 @@
 package com.example.androidproject.settings
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.androidproject.R
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
 import com.example.androidproject.databinding.ActivitySettingsBinding
+
+
+val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
 
 class SettingsActivity : AppCompatActivity() {
 
