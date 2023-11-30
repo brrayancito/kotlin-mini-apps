744afa0f60ad82fdff2552042ad3e81fb4e4f05b
Brrayan <brrayanperez10@gmail.com>
2023-11-30 19:24:33 -0400

    Saving settings data in dataStore

--- app/src/main/java/com/example/androidproject/settings/SettingsActivity.kt
+++ app/src/main/java/com/example/androidproject/settings/SettingsActivity.kt
@@ -5,13 +5,22 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.preferencesDataStore
 import com.example.androidproject.databinding.ActivitySettingsBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 
 val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
 
 class SettingsActivity : AppCompatActivity() {
+
+    companion object {
+        const val KEY_DARK_MODE = "key_dark_mode"
+    }
 
     private lateinit var binding: ActivitySettingsBinding
 
@@ -20,5 +29,18 @@ class SettingsActivity : AppCompatActivity() {
         binding = ActivitySettingsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        initUI()
+    }
+
+    private fun initUI() {
+        binding.switchDarkMode.setOnCheckedChangeListener { _, value ->
+            CoroutineScope(Dispatchers.IO).launch { saveSettings(KEY_DARK_MODE, value) }
+        }
+    }
+
+    private suspend fun saveSettings(key: String, value: Boolean) {
+        dataStore.edit { preferences ->
+            preferences[booleanPreferencesKey(key)] = value
+        }
     }
 }
